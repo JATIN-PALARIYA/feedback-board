fcbbe14b43ef71b9b49b58094d38ea7664110234
Jatin Palariya <jatinpalariya507@gmail.com>
2025-08-06 13:49:27 +0530

    navbar added

--- app/page.js
+++ app/page.js
@@ -2,26 +2,30 @@ import { connectDB } from "@/app/lib/mongodb";
 import Feedback from "@/app/models/Feedback";
 import Link from "next/link";
 import FeedbackForm from "./components/FeedbackForm";
+import Navbar from "./components/Navbar";
 
 export default async function Home() {
   await connectDB();
   const feedbackList = await Feedback.find().sort({ createdAt: -1 });
   return (
-    <main className="bg-gray-500">
-      <h1>All feedbacks</h1>
-      <div>
-        <FeedbackForm />
-      </div>
-      <div>
-        {feedbackList.map((feedback) => (
-          <div key={feedback._id}>
-            <Link href={`/feedback/${feedback._id}`}>
-              <h3>{feedback.title}</h3>
-              <p>{feedback.description?.slice(0, 100)}...</p>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </main>
+    <div>
+      <Navbar />
+      <main className="bg-gray-500">
+        <h1>All feedbacks</h1>
+        <div>
+          <FeedbackForm />
+        </div>
+        <div>
+          {feedbackList.map((feedback) => (
+            <div key={feedback._id}>
+              <Link href={`/feedback/${feedback._id}`}>
+                <h3>{feedback.title}</h3>
+                <p>{feedback.description?.slice(0, 100)}...</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
